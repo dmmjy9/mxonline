c75daf094120d528f5328cd4776132b63e1c759d
dmmjy9 <dmmjy9@live.cn>
2017-10-15 22:49:48 +0800

    user_register_step1

--- apps/users/views.py
+++ apps/users/views.py
@@ -4,9 +4,9 @@ from django.contrib.auth import authenticate, login
 from django.contrib.auth.backends import ModelBackend
 from django.db.models import Q
 from django.views.generic.base import View
-
+from django.contrib.auth.hashers import make_password
 from .models import UserProfile
-from .forms import LoginForm
+from .forms import LoginForm, RegisterForm
 
 # Create your views here.
 
@@ -31,6 +31,23 @@ class LoginView(View):
             if user is not None:
                 login(request, user)
                 return render(request, "index.html")
+            else:
+                return render(request, "login.html", {"msg": "用户名或密码错误!", "login_form": login_form})
         else:
-            return render(request, "login.html", {"msg": "用户名或密码错误!"})
+            return render(request, "login.html", {"login_form": login_form})
 
+
+class RegisterView(View):
+    def get(self, request):
+        register_form = RegisterForm()
+        return render(request, "register.html", {'register_form': register_form})
+    def post(self, request):
+        register_form = RegisterForm(request.POST)
+        if register_form.is_valid():
+            user_name = request.POST.get("username", "")
+            pass_word = request.POST.get("password", "")
+            user_profile = UserProfile()
+            user_profile.username = user_name
+            user_profile.email = user_name
+            user_profile.password = make_password(pass_word)
+            user_profile.save()
